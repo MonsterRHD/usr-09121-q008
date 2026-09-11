28fbdfdc169e53b342dd5329d8d1a2ff7d18bc29
ding <ding@ding.com>
2026-09-11 20:31:59 +0800

    补齐后端技术基线

--- mix.exs
+++ mix.exs
@@ -1,6 +1,13 @@
 defmodule BackendService.MixProject do
   use Mix.Project
 
-  def project, do: [app: :backend_service, version: "0.1.0"]
+  def project, do: [app: :risk_notice_service, version: "0.1.0", deps: deps()]
   def application, do: [extra_applications: [:logger]]
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.7"},
+      {:redix, "~> 1.5"}
+    ]
+  end
 end
